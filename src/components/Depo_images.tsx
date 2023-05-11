bd4c2e774fe2ee3b268e905eb355d76b0c0cc539
Israel-Felipe <israelfelipet@gmail.com>
2023-05-11 15:20:57 -0300

    feat: add button in depo-images and change transitions

--- src/components/Depo_images.tsx
+++ src/components/Depo_images.tsx
@@ -9,7 +9,7 @@ import wallpaper from "../images/sombra_planta.svg";
 
 export default function DepoImages() {
   return (
-    <motion.header className="relative flex justify-center h-auto bg-[#943A49] z-0 overflow-hidden">
+    <motion.header className="relative flex flex-col justify-center items-center h-auto bg-[#943A49] z-0 overflow-hidden">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 100 }}
@@ -31,7 +31,7 @@ export default function DepoImages() {
         <motion.div
           initial="hidden"
           whileInView="show"
-          variants={textTransition(0.6)}
+          variants={fadeIn("right", "spring", 0.4, 1)}
           className="flex justify-around m-auto gap-6"
         >
           {Depos_content.map((content, i) => (
@@ -45,7 +45,31 @@ export default function DepoImages() {
             />
           ))}
         </motion.div>
+
+        <motion.div
+          initial="hidden"
+          whileInView="show"
+          className="relative flex w-full justify-center items-center mt-12 w-[60%] h-auto m-auto z-10"
+          variants={textTransition(0.6)}
+        >
+          <Image
+            src={sombra}
+            className={`absolute left-1/2 transform -translate-x-1/2 top-[95%] w-[97%] h-auto -z-10
+          }`}
+            alt="sombra"
+          />
+          <a href="#">
+            <button
+              className=" bg-[#FEF4E6] hover:bg-[#E4AC80] active:bg-[#E4795B] text-[#751133] active:text-[#FEF4E6] h-24 py-2 px-4 rounded-full font-bold text-[34px] z-10
+            transition duration-200 transform hover:scale-110 active:scale-100 w-[780px]"
+            >
+              QUERO SER UMA TAGARELA!
+            </button>
+          </a>
+        </motion.div>
       </motion.nav>
+
+      <motion.div className="bg-blue-300 w-full"></motion.div>
     </motion.header>
   );
 }
@@ -69,16 +93,21 @@ function BlocsDepos({
         src={image}
         className={`w-full h-auto rounded-lg
           }`}
-        alt="wallpaper"
+        alt="depoimento"
       />
 
-      <div className="absolute bottom-0 flex gap-4 bg-white bg-opacity-70 w-full p-4 rounded-lg">
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        className="absolute bottom-0 flex gap-4 bg-white bg-opacity-70 w-full p-4 rounded-lg"
+        variants={textTransition(0.3)}
+      >
         <div>
           <Image
             src={foto}
             className={`w-20 h-auto rounded-full
           }`}
-            alt="wallpaper"
+            alt="foto"
           />
         </div>
         <div className="flex flex-col justify-center">
@@ -86,7 +115,7 @@ function BlocsDepos({
           <p>{idade}</p>
           <p>{cidade}</p>
         </div>
-      </div>
+      </motion.div>
     </nav>
   );
 }
