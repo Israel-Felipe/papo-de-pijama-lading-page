a2f7adb51189ff7fc3c73834f9ef20d611a275e3
Israel-Felipe <israelfelipet@gmail.com>
2023-05-30 16:52:40 -0300

    add scripts for pixel facebook and google manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,11 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from "next/document";
+import Image from "next/image";
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400;1,500;1,700&display=swap"
-          rel="stylesheet"
-        />
-
         {/* Início do código do pixel do Facebook */}
         <script
           dangerouslySetInnerHTML={{
@@ -29,18 +23,50 @@ export default function Document() {
             `,
           }}
         />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=785607259813837&ev=PageView&noscript=1"
-          />
-        </noscript>
         {/* Fim do código do pixel do Facebook */}
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W6C9W86');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <body>
+        <Image
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=785607259813837&ev=PageView&noscript=1"
+          alt="logo"
+        />
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-W6C9W86"
+          height="0"
+          width="0"
+          style={{ display: "none" }}
+        ></iframe>
         <Main />
         <NextScript />
       </body>
